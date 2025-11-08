933379fca4d8ed6b2be6c19d9a30f9d914ca6184
Muhammad Usman Awan <uawan8154@gmail.com>
2025-11-08 18:38:18 +0500

    feat: add riddleMode in userData store

--- src/store/app/userData.ts
+++ src/store/app/userData.ts
@@ -4,9 +4,11 @@ import { persist } from "zustand/middleware";
 interface UserStore {
   username: string;
   isHost: boolean;
+  riddleMode: string;
 
   setUsername: (username: string) => void;
   setIsHost: (value: boolean) => void;
+  setRiddleMode: (mode: string) => void;
   resetUser: () => void;
 }
 
@@ -15,9 +17,11 @@ export const useUserStore = create<UserStore>()(
     (set) => ({
       username: "",
       isHost: false,
+      riddleMode: "riddle",
 
       setUsername: (username) => set({ username }),
       setIsHost: (value) => set({ isHost: value }),
+      setRiddleMode: (mode) => set({ riddleMode: mode }),
 
       resetUser: () => set({ username: "", isHost: false }),
     }),
